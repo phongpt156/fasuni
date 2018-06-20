4dc648cf9d5b1d4057f12cbe09b4e76fe960f894
Phong Phan <pthanhphong156@gmail.com>
2018-06-20 11:32:23 +0700

    fix whislist page

--- fasuni/src/components/my-whistlist/ProductCard.vue
+++ fasuni/src/components/my-whistlist/ProductCard.vue
@@ -3,9 +3,9 @@
     <div class="product-card h-100">
       <div class="card h-100">
         <div class="card-header p-0">
-          <a href="/product/245?color=38" class="image-wrapper image-standard d-block">
+          <router-link :to="{name: 'Product', params: {id: product.master_product_id || product.id}, query: product.color ? {color: product.color.id} : {}}" class="image-wrapper image-standard d-block">
             <img alt="Sơ mi T-283-S" class="card-img-top img-fluid" :src="image.original" />
-          </a>
+          </router-link>
         </div>
         <div class="card-body px-1 py-1 text-center">
           <div class="text-uppercase name py-3">{{ product.name }}</div>
@@ -42,12 +42,12 @@ export default {
         return this.product.images[0];
       }
 
-      if (this.product.master_product && this.product.master_product.images.length && this.product.master_product.color.length && this.product.master_product.color[0].id === this.color.id) {
+      if (this.product.master_product && this.product.master_product.images.length && this.product.master_product.color && this.product.master_product.color.id === this.color.id) {
         return this.product.master_product.images[0];
       }
 
       for (const subProduct of this.product.sub_products) {
-        if (subProduct.images.length && subProduct.color.length && subProduct.color[0].id === this.color.id) {
+        if (subProduct.images.length && subProduct.color && subProduct.color.id === this.color.id) {
           return subProduct.images[0];
         }
       }
@@ -55,8 +55,8 @@ export default {
       return '';
     },
     color() {
-      if (this.product.color.length) {
-        return this.product.color[0];
+      if (this.product.color) {
+        return this.product.color;
       }
 
       return null;
